03480c6923aaeb80c07b9d90c85f6b10e920f9c9
cryptomehedi <classicalmehedi@gmail.com>
2022-07-12 01:20:05 +0600

    reg update

--- src/Components/LoginRed/Reg.js
+++ src/Components/LoginRed/Reg.js
@@ -12,6 +12,7 @@ const Reg = () => {
     const [emailErr, setEmailErr] = useState('')
     const [passErr, setPassErr] = useState('')
     const [cPassErr, setCPassErr] = useState('')
+    const [check, setCheck] = useState(false)
 
     const location = useLocation()
     const from = location.state?.from?.pathname || "/profile";
@@ -47,19 +48,22 @@ const Reg = () => {
             setCPassErr('Confirm Password Must Be Required')
         }else if(displayName.length > 0 && email.length > 0 && password.length > 5){
             if(password === confirm) {
-                // await createUserWithEmailAndPassword(email, password)
-                // await updateProfile({displayName})
-                toast.success(`welcome ${displayName}`)
+                await createUserWithEmailAndPassword(email, password)
+                await updateProfile({displayName})
             }else{
                 clear()
                 setCPassErr('password did not match')
             }
         }
     }
+    if(error?.message?.length > 4){
+        toast.error(`${error.message.slice(22,error.message.length -2)}`)
+    }
     if(user){
+        toast.success(`welcome ${user.user.displayName}`)
         navigate(from, { replace: true })
     }
-
+    console.log(check);
     return (
         <div className="flex justify-center mt-10">
             <div className='w-96'>
@@ -85,13 +89,13 @@ const Reg = () => {
                         <input className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500' type="password" name="ConfirmPassword" id="ConfirmPassword" placeholder="Confirm Password" />
                         {cPassErr && <p className='text-red-500 text-sm'>{cPassErr}</p>}
                     </div>
-                    <input className='bg-transparent text-black border-2 hover:bg-green-400 focus:outline-none focus:ring focus:ring-blue-300 active:bg-green-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold hover:text-white cursor-pointer duration-200 mt-3' type="submit" value="Sign Up" />
-                    {
-                        error && <p>{error.message.slice(17, error.message.length-1)}</p>
-                    }
-                    {
-                        loading && <Spinner/>
-                    }
+                    {/* <input type="checkbox" /> */}
+                    <div className="flex items-center mt-1" >
+                        <input onClick={()=> setCheck(!check)} id="link-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                        <label htmlFor="link-checkbox" className="ml-2 text-sm font-medium text-gray-600">I agree with the <span className="text-blue-600 dark:text-blue-500 hover:underline">terms and conditions</span>.</label>
+                    </div>
+                    <input disabled={!check} className={!check ? 'bg-transparent text-black border-2 cursor-not-allowed hover:bg-gray-400 px-5 py-2 text-sm leading-5 rounded-full font-semibold duration-200 mt-3' : 'bg-transparent text-black border-2 hover:bg-green-400 focus:outline-none focus:ring focus:ring-blue-300 active:bg-green-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold hover:text-white cursor-pointer duration-200 mt-3'} type="submit" value="Sign Up" />
+                    {loading && <div className="mt-2"><Spinner text="Please Wait For Registration Process...."/></div>}
                 </form>
                 <h3 className='mt-3'>Already Registered! Please <Link className='text-green-400' to="/Login"> Login Here</Link></h3>
             </div>
